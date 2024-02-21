b657ddf003085219524fc1160532ea0a3cda9e03
Yafik <mhmmdyfik@gmail.com>
2024-02-22 05:35:46 +0700

    Hardcoded search local json

--- components/ui/LyricList.tsx
+++ components/ui/LyricList.tsx
@@ -1,139 +1,55 @@
 import { Card, CardHeader, Divider, CardBody } from "@nextui-org/react";
+import data from "@/api/lyrics.json";
+
+type Lyric = {
+  lyric: string;
+  prev: string;
+  next: string;
+  multiplicity: number;
+};
+
+type Song = {
+  [songTitle: string]: Lyric[];
+};
+
+type Album = {
+  [albumTitle: string]: Song;
+};
 
 export default function LyricList() {
-  const lyricsJSON = require("/hindia/lyrics.json");
+  const albums: Album = data;
+  const searchTerm = "malam";
 
   return (
-    <>
-      <Card className="mt-4 max-h-[800px] max-w-[800px] p-3">
-        <CardHeader className="flex gap-3">
-          <div className="flex flex-col">
-            <p className="text-md font-medium">Found 18 usages in 13 songs</p>
-          </div>
-        </CardHeader>
-        <Divider />
-        <CardBody>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-            nihil laboriosam aspernatur sed illum aperiam similique suscipit
-            ipsam, repellat laudantium temporibus doloribus nostrum hic
-            voluptatibus nemo impedit. Error animi beatae tenetur, enim debitis
-            quaerat quam quia fugit modi porro earum voluptatibus cupiditate
-            alias praesentium. Repellat iste incidunt voluptatem distinctio odit
-            ipsa labore cumque consequuntur totam alias mollitia aliquam non
-            corporis obcaecati eius eveniet reprehenderit recusandae iusto
-            officia modi architecto, enim, optio vitae officiis. Tempora natus
-            perspiciatis magnam dolores sequi, omnis expedita modi nulla
-            distinctio? Deleniti sint adipisci sapiente culpa laborum enim rem
-            nobis molestiae veniam quia quo ipsum sit repellat labore, esse
-            temporibus tempora exercitationem totam quasi tenetur consectetur.
-            Et quia dicta numquam minus voluptatem veniam ab facilis ipsum iusto
-            architecto cupiditate error, officia repellendus deserunt nulla
-            dolorum labore iste modi! Accusantium odio dolore nemo corporis
-            voluptatum! Ipsa aliquam nam repellat laudantium velit hic incidunt
-            delectus vel quidem, reprehenderit sunt reiciendis perspiciatis
-            molestiae repellendus, mollitia consectetur consequuntur? Expedita,
-            atque inventore. Sunt vel eos eum consectetur perferendis cum
-            similique repellendus dolore delectus ea laboriosam tempore qui ut
-            soluta velit fugiat ratione doloremque sed dicta facere, id
-            accusantium? Dolorem sed earum quia enim nemo, tempora expedita
-            totam consequuntur vel beatae eos cupiditate fugiat, accusamus id
-            facilis maxime impedit natus vitae vero molestiae quisquam
-            obcaecati. Dolorem, non. Velit quos cum quo eius reprehenderit
-            adipisci eveniet dolorem natus eaque. Odio iste esse assumenda
-            soluta nihil dicta libero at et, quod eos amet ex delectus alias nam
-            veritatis mollitia qui eum neque corrupti est enim. Quae modi qui
-            voluptatem omnis. Soluta quidem distinctio minima pariatur magni!
-            Numquam sunt earum repellendus, assumenda eius voluptatibus
-            voluptatum sequi dignissimos voluptatem, molestias impedit excepturi
-            quaerat! Vel aut, fuga consequatur optio incidunt ex corporis
-            ducimus dicta placeat autem temporibus dolorum accusamus nulla
-            cumque unde nihil ut, cupiditate consequuntur nisi nostrum
-            praesentium? Omnis hic sapiente fugiat eligendi, quia numquam in
-            atque nobis molestias quaerat sed, excepturi pariatur voluptate
-            perferendis dolore. Provident officia consequuntur odit
-            necessitatibus eligendi voluptate consectetur, unde iure alias
-            animi, quia minima repellat sed temporibus natus neque magni esse
-            ratione corporis odio dolore sit. Accusamus corporis sapiente
-            commodi atque quod consectetur reprehenderit odit suscipit earum.
-            Illo, architecto alias nisi sed expedita iste dolore soluta dolor
-            eius, error maiores temporibus, nulla laboriosam corrupti deleniti
-            doloremque consequatur voluptatem aperiam porro! Maxime consectetur
-            vero neque, architecto debitis sunt est voluptas ab adipisci,
-            deserunt natus veniam unde voluptatibus. Odit facilis adipisci
-            officiis amet error nemo eum delectus iusto ipsum sunt maxime nulla
-            recusandae quas, corrupti, nihil illo placeat neque rem ab quos
-            dolorum, repellendus maiores. Deserunt, ipsam iusto alias
-            repellendus earum, eveniet consequatur voluptatem sunt sed numquam
-            vitae. Blanditiis ex incidunt labore animi ipsum laboriosam nam
-            exercitationem recusandae! Quam hic officiis aspernatur mollitia
-            eius voluptates non labore minima ratione harum repellendus odio,
-            ipsam molestias. Cum sequi et nihil perspiciatis! Quas voluptas,
-            aspernatur ab ducimus quia maxime provident magni quaerat excepturi
-            ipsam harum soluta quod natus in vitae similique mollitia
-            doloremque, distinctio nostrum blanditiis eos? Omnis, reiciendis
-            sapiente molestias adipisci tempora deserunt, doloremque, nesciunt
-            enim culpa esse illo vitae perspiciatis. Veritatis nihil dolore
-            placeat eligendi incidunt excepturi fuga explicabo eveniet ipsa!
-            Consequuntur nobis voluptate magnam voluptatibus blanditiis a. Ut
-            maiores cupiditate ab iure eos excepturi. Odit excepturi deserunt
-            alias nemo officia atque inventore quam voluptate obcaecati. Quam
-            quae sequi, recusandae et expedita, libero illo nulla minima
-            voluptatem distinctio sint illum, impedit iure dolor sed totam enim
-            omnis sapiente quidem culpa iusto voluptates? Dolorum vel obcaecati
-            veniam eligendi? Impedit eaque nostrum, aperiam quo perferendis illo
-            obcaecati voluptatum dolore ipsa rem earum ad labore modi facere
-            facilis enim accusantium illum possimus optio aliquid quia deleniti
-            quod quaerat. Optio possimus recusandae unde vero voluptate
-            perspiciatis, laudantium expedita eos quod, a ipsam laborum
-            aspernatur magni ad debitis explicabo dolor commodi libero
-            necessitatibus. Vero dolorum error porro fugiat placeat ipsa, omnis
-            recusandae obcaecati atque impedit accusantium minus odio architecto
-            ex pariatur consectetur illo alias. Rem illo fugiat, quas saepe
-            culpa, et reiciendis a, consectetur dolorum eum doloremque nulla
-            soluta praesentium quisquam commodi. Eos labore voluptatem
-            blanditiis quisquam quidem error consequuntur. Nulla dolores dicta
-            voluptatem voluptas accusantium ex sed pariatur aspernatur rem, quas
-            amet inventore quos distinctio dolorum! Nobis sunt in voluptatem
-            itaque iste culpa facere alias reiciendis, eveniet illo iusto a
-            vitae nihil optio nulla laborum! Adipisci aperiam saepe eaque
-            obcaecati quibusdam consequatur, fugiat fugit a tenetur eum quaerat,
-            sapiente animi maxime! Magni maiores sit nam voluptate, optio modi
-            maxime est architecto unde sapiente, asperiores quasi at error amet
-            velit provident vel ad assumenda esse ratione quidem. Porro
-            voluptatibus vero dolorum reiciendis repellat? Officia consequatur
-            ipsa laborum veniam quisquam corrupti temporibus porro consequuntur
-            assumenda dolorem officiis at perferendis tenetur, consectetur quasi
-            quod autem aspernatur voluptatibus ab earum ratione excepturi.
-            Laborum a explicabo saepe porro sunt eos quos. In impedit itaque
-            voluptates adipisci voluptatum nulla, ut incidunt blanditiis soluta
-            deleniti at hic id laboriosam reprehenderit dolore, fugiat alias.
-            Illum quidem corrupti aspernatur molestias labore minus recusandae
-            eum non modi soluta dolorum at necessitatibus sit quaerat fuga
-            possimus vel, totam tempora quas quis ratione repudiandae. Itaque
-            quas blanditiis ea assumenda nisi quis quos quae. Accusamus
-            doloremque quasi, harum adipisci voluptatem earum error
-            necessitatibus dolorem maiores commodi sint sit, deserunt sapiente
-            tenetur, in obcaecati possimus delectus dolores modi aut alias
-            consectetur? Porro quis, architecto praesentium numquam repellat
-            quisquam optio officiis! Sequi itaque blanditiis perferendis quod
-            sapiente earum qui impedit eveniet modi ratione. Tenetur doloribus
-            nemo amet libero. Sunt possimus quas laudantium fuga numquam animi
-            ut similique, laborum itaque deleniti nostrum doloremque sint. Optio
-            fugiat deserunt, dolor officia pariatur, aliquam atque vitae, enim
-            vel aspernatur consequatur facere? Aliquid laborum quibusdam libero
-            blanditiis perferendis similique beatae, officiis cum assumenda qui
-            esse, illum nam quae minus reiciendis corrupti aut atque aspernatur.
-            Laboriosam nam consectetur ab temporibus possimus facere, recusandae
-            sunt modi delectus exercitationem error rem nulla nisi quae corporis
-            quidem aliquam accusantium doloremque, quod illo repudiandae unde
-            porro? Numquam excepturi quasi iste mollitia. Quis quo pariatur
-            sequi provident explicabo, atque, tempora animi maxime, fuga magnam
-            perspiciatis consequuntur. Quaerat, exercitationem in aperiam
-            laboriosam debitis suscipit tempore.
-          </p>
-        </CardBody>
-      </Card>
-    </>
+    <Card className="mt-4 max-h-[800px] max-w-[800px] p-3">
+      <CardHeader className="flex gap-3">
+        <div className="flex flex-col">
+          <p className="text-md font-medium">Found 46 usages in 24 songs</p>
+        </div>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        {Object.entries(albums).map(([albumTitle, songs]) =>
+          Object.entries(songs).map(([songTitle, lyrics]) =>
+            lyrics
+              .filter((lyric) => lyric.lyric.includes(searchTerm))
+              .map((lyric, index) => (
+                <>
+                  <div className="py-3" key={index}>
+                    <p>{lyric.prev}</p>
+                    <p>{lyric.lyric}</p>
+                    <p>{lyric.next}</p>
+                    <br />
+                    <p>
+                      {songTitle}, {albumTitle}
+                    </p>
+                  </div>
+                  <Divider />
+                </>
+              )),
+          ),
+        )}
+      </CardBody>
+    </Card>
   );
 }
